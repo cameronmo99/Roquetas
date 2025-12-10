51a355dc1760534b86a497fd1ab340be63022177
Cameron Morris <cameronmo99@gmail.com>
2025-12-10 14:24:33 +0000

    still getting errors when pressing the button, is this the problem? Erro

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,7 +10,6 @@ export default createMiddleware({
 });
  
 export const config = {
-  // Skip all paths that should not be internationalized. This example skips the
-  // folders "api", "_next" and all files with an extension (e.g. favicon.ico)
-  matcher: ['/((?!api|_next|.*\\..*).*)']
+  // Match only internationalized pathnames
+  matcher: ['/', '/(de|en|es|fr|ca)/:path*']
 };
